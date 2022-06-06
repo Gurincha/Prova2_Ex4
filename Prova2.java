993328d6c28767b27edd00c3b4e51fa30372f868
Gurincha <gustavo.rincha@fatec.sp.gov.br>
2022-06-06 17:01:00 +0000

    att

---
+++ Prova2.java
@@ -0,0 +1,10 @@
+public class Prova2{
+    public static void main(String[] args) {
+
+        
+        for (int i = 38; i < 1001; i++) {
+            System.out.print(i+"; ");
+            
+        }
+    }
+}
